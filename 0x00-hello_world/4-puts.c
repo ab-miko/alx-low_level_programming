d15416783999941c22cd6594ca6e83870c039588
Mickey <mikyasabadirz@gmail.com>
2022-12-08 06:30:59 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+
+
 /**
  * main - Entry point
  *
@@ -6,7 +8,6 @@
  */
 int main(void)
 {
-	char Programming[] = "Programming is like building a multilingual puzzle";
-	puts(Programming);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
